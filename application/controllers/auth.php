84c1a64866be4697509a45c354c8e8b2495d6270
yedukondaluvanipenta <yedukondalu.vanipenta@patlive.com>
2014-01-22 11:48:12 +0530

    Signed-off-by: yedukondaluvanipenta <yedukondalu.vanipenta@patlive.com>

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -9,29 +9,28 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->login();
+        if (!$this->input->is_ajax_request()) {
+            $this->status = 'error';
+            $this->message = 'Request type not supported';
+            return $this->sendResponse();
+        }
     }
 
     public function login()
     {
-        if ($this->input->is_ajax_request()) {
-            $this->load->model('users');
-            $username = $this->input->post('email');
-            $password = $this->input->post('password');
-            $userExist = $this->users->findUserByEmailAndPassword($username, $password);
-            if ($userExist) {
-                $this->session->set_userdata('user', $userExist);
-                $this->status = 'success';
-                $this->message = 'User logged in successfully ';
-                $this->data = $userExist;
-            } else {
-                $this->status = 'error';
-                $this->message = 'User not exists';
-                $this->data = $userExist;
-            }
+        $this->load->model('users');
+        $username = $this->input->post('email');
+        $password = $this->input->post('password');
+        $userExist = $this->users->findUserByEmailAndPassword($username, $password);
+        if ($userExist) {
+            $this->session->set_userdata('user', $userExist);
+            $this->status = 'success';
+            $this->message = 'User logged in successfully';
+            $this->data = $userExist;
         } else {
             $this->status = 'error';
-            $this->message = 'Request type not supported';
+            $this->message = 'User not exists';
+            $this->data = $userExist;
         }
         return $this->sendResponse();
     }
